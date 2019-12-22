eb470d32115f006c1c2dca0c5d8bfb3cfbaaf062
Andrew Regan <aregan@gmail.com>
2019-12-22 22:09:46 +0000

    Detect 4 Assertion libs

--- src/main/java/uk/co/crunch/platform/handlers/TestHandler.java
+++ src/main/java/uk/co/crunch/platform/handlers/TestHandler.java
@@ -2,33 +2,40 @@ package uk.co.crunch.platform.handlers;
 
 import org.apache.commons.lang3.StringUtils;
 import org.objectweb.asm.Opcodes;
-import uk.co.crunch.platform.asm.ClassAnnotationVisitor;
-import uk.co.crunch.platform.asm.ClassDefinitionVisitor;
-import uk.co.crunch.platform.asm.MethodAnnotationVisitor;
-import uk.co.crunch.platform.asm.MethodDefinitionVisitor;
+import uk.co.crunch.platform.asm.*;
 import uk.co.crunch.platform.exceptions.CrunchRuleViolationException;
 import uk.co.crunch.platform.maven.CrunchServiceMojo;
+
+import java.util.EnumSet;
+
+import static java.util.stream.Collectors.joining;
 
 public class TestHandler implements HandlerOperation {
     @Override
     public void run(CrunchServiceMojo mojo) {
 
+        var visitor = new Vis(mojo);
         var time = System.currentTimeMillis();
         try {
-            mojo.analyseCrunchClasses(() -> false, new Vis(mojo));
+            mojo.analyseCrunchClasses(() -> false, visitor);
         } finally {
             var newTime = System.currentTimeMillis();
             mojo.getLog().info("Test analysis completed in " + (newTime - time) + " msecs");
+            mojo.getLog().info("Assertion types in use: " + visitor.assertionTypes.stream().map(each -> each.displayValue).collect(joining(",")));
         }
     }
 
-    private static class Vis implements ClassDefinitionVisitor, ClassAnnotationVisitor, MethodAnnotationVisitor, MethodDefinitionVisitor {
+    private static class Vis implements ClassDefinitionVisitor, ClassAnnotationVisitor, MethodAnnotationVisitor, MethodDefinitionVisitor, MethodCallVisitor {
 
         private final CrunchServiceMojo mojo;
 
         private boolean isKotlin;
         private boolean foundJUnit4;
         private boolean foundJUnit5;
+
+        private final EnumSet<AssertionType> assertionTypes = EnumSet.noneOf(AssertionType.class);
+        private boolean shownKotlinAssertJMigrateWarning;
+
         private boolean isMethodPublic;
         private boolean isMethodPrivate;
 
@@ -73,6 +80,21 @@ public class TestHandler implements HandlerOperation {
                     mojo.getLog().info("Java Test: " + className + " : " + name);
                 }
 
+                if (this.assertionTypes.contains(AssertionType.JUNIT4)) {
+                    throw new CrunchRuleViolationException("We should stop using JUnit4 assertions");
+                }
+
+                if (this.assertionTypes.contains(AssertionType.HAMCREST)) {
+                    throw new CrunchRuleViolationException("We should stop using Hamcrest");
+                }
+
+                if (isKotlin) {
+                    if (this.assertionTypes.contains(AssertionType.ASSERTJ) && !this.shownKotlinAssertJMigrateWarning) {
+                        mojo.getLog().warn("Kotlin tests ought to start moving from AssertJ => Strikt, where possible");
+                        this.shownKotlinAssertJMigrateWarning = true;
+                    }
+                }
+
                 if (!this.shownClassPublicWarning && isClassPublic && !isKotlin) {
                     mojo.getLog().warn("Java test class `" + displayClassName(className) + "` does not need to be public");
                     this.shownClassPublicWarning = true;
@@ -94,6 +116,37 @@ public class TestHandler implements HandlerOperation {
             } else if ((access & Opcodes.ACC_PRIVATE) != 0) {
                 isMethodPrivate = true;
             }
+        }
+
+        @Override
+        public void visitMethodCall(String className, String owner, String name, String desc) {
+//            System.out.println(owner + " / " + name);
+
+            if (owner.equals("org/junit/Assert")) {
+                this.assertionTypes.add(AssertionType.JUNIT4);
+            }
+
+            if (owner.startsWith("org/hamcrest/MatcherAssert")) {
+                this.assertionTypes.add(AssertionType.HAMCREST);
+            }
+
+            if (owner.startsWith("strikt/api")) {
+                this.assertionTypes.add(AssertionType.STRIKT);
+            }
+
+            if (owner.startsWith("org/assertj")) {
+                this.assertionTypes.add(AssertionType.ASSERTJ);
+            }
+        }
+    }
+
+    private enum AssertionType {
+        HAMCREST("Hamcrest"), ASSERTJ("AssertJ"), JUNIT4("JUnit4"), STRIKT("Strikt");
+
+        final String displayValue;
+
+        AssertionType(String displayValue) {
+            this.displayValue = displayValue;
         }
     }
 
